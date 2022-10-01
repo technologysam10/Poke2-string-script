cfb994b175bddf92971a1ca5915e09c874cbb17b
technologysam <technologysam@outlook.com>
2022-10-01 16:39:46 -0400

    Change time between

--- script.py
+++ script.py
@@ -1,23 +1,30 @@
+import time
 import random
 import keyboard
+
 print("Welcome to poke2 script.")
+
+
 
 #Get a random string
 
-random_string = ''
- 
-for _ in range(10):
-    # Considering only upper and lowercase letters
-    random_integer = random.randint(97, 97 + 26 - 1)
-    flip_bit = random.randint(0, 1)
-    # Convert to lowercase if the flip bit is on
-    random_integer = random_integer - 32 if flip_bit == 1 else random_integer
-    # Keep appending random characters using chr(x)
-    random_string += (chr(random_integer))
- 
-print(random_string, len(random_string))
 
-#type it
 
-keyboard.write(random_string, delay=0.1)
+printit = input("Should It print each string along with typeing it (True/False)")
 
+isOn = True
+
+        
+
+while isOn:
+    random_string = ''
+    for _ in range(30):
+        # Considering only upper and lowercase letters
+        random_integer = random.randint(97, 97 + 26 - 1)
+        flip_bit = random.randint(0, 1)
+        # Convert to lowercase if the flip bit is on
+        random_integer = random_integer - 32 if flip_bit == 1 else random_integer
+        # Keep appending random characters using chr(x)
+        random_string += (chr(random_integer))
+    keyboard.write(random_string, delay=0.15)
+    keyboard.press_and_release("enter")
